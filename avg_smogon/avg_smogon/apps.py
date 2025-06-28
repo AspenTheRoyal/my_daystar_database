0ff27a097407740ea55706e5ad44fabd52b216c8
AspenTheRoyal <ariatheroyal@outlook.com>
2025-06-27 21:13:16 -0500

    Delete avg_smogon directory because it was broke

--- avg_smogon/avg_smogon/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AvgSmogonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'avg_smogon'
--- avg_smogon/avg_smogon/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('avg_smogon/', views.avg_smogon_view, name='avg_smogon'),
-    path('my_smogon_script', views.my_smogon_script, name='my_smogon_script'),
-]
